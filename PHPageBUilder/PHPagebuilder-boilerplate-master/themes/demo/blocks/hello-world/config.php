b928fbc625faba36a43199715698d52116cbc4bf
dschiffer12 <dschiffer12@gmail.com>
2023-02-23 16:55:04 -0700

    Reset

--- PHPageBUilder/PHPagebuilder-boilerplate-master/themes/demo/blocks/hello-world/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'title' => 'Hello World',
-    'category' => 'General',
-    'icon' => 'fa fa-hand-peace-o',
-];
--- PHPageBUilder/PHPagebuilder-boilerplate-master/themes/demo/layouts/master/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'title' => 'Master Layout',
-];
