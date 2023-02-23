bdb987da6048ce2cf0b243a2fd5d57a3566ece9a
dschiffer12 <dschiffer12@gmail.com>
2023-02-23 16:50:05 -0700

    Composer

---
+++ PHPageBUilder/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hansschouten/phpagebuilder',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '06d525e7ff6e8d31773063070e8a3e6f16a50d6f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hansschouten/phpagebuilder' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '06d525e7ff6e8d31773063070e8a3e6f16a50d6f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
